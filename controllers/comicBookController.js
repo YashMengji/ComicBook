90f4b78fb88c2436ed31e54adf090219cb1f20bb
YashMengji <yashmengji2172005@gmail.com>
2024-10-17 23:01:00 +0530

    Modified Read functionality with filters

--- controllers/comicBookController.js
+++ controllers/comicBookController.js
@@ -13,13 +13,56 @@ async function createComicBook(req, res){
 // Controller to get all comic books
 async function getComicBooks(req, res){
   try {
-    const comicBooks = await comicModel.find();
+    // These params would be passed by axios -> options field by frontend
+    const { page = 1, limit = 10, authorName, yearOfPublication, price, condition, sortBy = 'bookName', order = 'asc' } = req.query;
+    const pageLimit = parseInt(limit);
+    const pageNumber = parseInt(page);
+    
+    const filter = {};
+    if(authorName) filter.authorName = authorName;
+    if(yearOfPublication) filter.yearOfPublication = yearOfPublication;
+    if(price) filter.price = price;
+    if(condition) filter.condition = condition;
+
+    const comicBooks = await comicModel.find(filter)
+        .sort({[sortBy]: order === 'asc' ? 1 : -1})
+        .limit(pageLimit)
+        .skip((pageNumber - 1) * pageLimit)
+    
     return res.status(200).json(comicBooks);
   } catch (error) {
     return res.status(500).json({message: error.message})
   }
 }
 
+// Controller to update comic book
+async function updateComicBook(req, res){
+  try {
+    const comicBook = await comicModel.findByIdAndUpdate(req.params.id, req.body, {new: true});
+    if(!comicBook){
+      return res.status(404).json({message: "Comic book not found"})
+    }
+    return res.status(200).json(comicBook);
+  }
+  catch (error) {
+    return res.status(500).json({message: error.message})
+  }
+}
+
+// Controller to delete comic book
+async function deleteComicBook(req, res){
+  try {
+    const comicBook = await comicModel.findByIdAndDelete(req.params.id);
+    if(!comicBook){
+      return res.status(404).json({message: "Comic book not found"})
+    }
+    return res.status(200).json({message: "Comic book deleted"});
+  }
+  catch (error) {
+    return res.status(500).json({message: error.message})
+  }
+}
+
 
 // Export all the controller methods as part of an object
-module.exports = {createComicBook, getComicBooks} // CommonJS method to export
+module.exports = {createComicBook, getComicBooks, updateComicBook, deleteComicBook} // CommonJS method to export
--- routes/comicBookRoutes.js
+++ routes/comicBookRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const {createComicBook, getComicBooks} = require("../controllers/comicBookController");
+const {createComicBook, getComicBooks, updateComicBook, deleteComicBook} = require("../controllers/comicBookController");
 const router = express.Router();
 
 // Prefixed by route mentioned in app.use()
 router.post("/", createComicBook);
-
 router.get("/", getComicBooks);
-
+router.put("/:id", updateComicBook);
+router.delete("/:id", deleteComicBook);
 
 module.exports = router;
